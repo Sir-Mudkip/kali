f6c8b54682465f3e61b48b7a76f687e207e382e4
Sir-Mudkip <lukechappman@gmail.com>
2026-02-05 23:58:53 +0000

    Updated Neovim Config

--- build_files/50-config.sh
+++ build_files/50-config.sh
@@ -12,3 +12,13 @@ touch ~/.hushlogin
 
 # Ignore case on tab completion
 echo "set completion-ignore-case On" >> /etc/inputrc
+
+# Neovim Plugin Configs
+mkdir -p /root/.local/share/nvim/site/pack/plugins/start && \
+cd /root/.local/share/nvim/site/pack/plugins/start && \
+git clone --depth 1 https://github.com/nvim-lua/plenary.nvim && \
+git clone --depth 1 https://github.com/nvim-telescope/telescope.nvim && \
+git clone --depth 1 https://github.com/catppuccin/nvim catppuccin && \
+git clone --depth 1 https://github.com/nvim-tree/nvim-web-devicons && \
+git clone --depth 1 https://github.com/nvim-neo-tree/neo-tree.nvim && \
+git clone --depth 1 https://github.com/MunifTanjim/nui.nvim
--- config/nvim/lua/luke/plug.lua
+++ config/nvim/lua/luke/plug.lua
@@ -3,21 +3,12 @@
 -- Ideally stick your functions on 1 line if you can
 -- If you need multiple then fine
 --
-local vim = vim
-local Plug = vim.fn['plug#']
+-- Telescope setup
+require('telescope').setup(require('telescope.themes').get_dropdown())
 
-Plug('nvim-lua/plenary.nvim')
-Plug('nvim-telescope/telescope.nvim')
-Plug('nvim-telescope/telescope-fzf-native.nvim', { ['do'] = 'make' })
-Plug('catppuccin/nvim', { as = 'catppuccin' })
-
-vim.call('plug#end')
-
--- Telescope setup (REQUIRED for Telescope to work)
-require('telescope').setup()
-
--- Catppuccin setup (REQUIRED for colorscheme to work)
+-- Catppuccin setup
 require('catppuccin').setup()
-
--- Set the colorscheme (REQUIRED to actually apply it)
 vim.cmd.colorscheme('catppuccin')
+
+-- Neo-tree setup (pass empty config table)
+require('neo-tree').setup({})
